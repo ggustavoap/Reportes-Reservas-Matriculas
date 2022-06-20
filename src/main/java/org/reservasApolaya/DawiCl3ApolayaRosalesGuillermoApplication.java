bfad2bc12cc7bb53093fbb106f55dbee592948dc
= <=>
2022-06-20 16:51:14 -0500

    Iniciando Proyecto Reserva Matricula - CL3 - Apolaya

---
+++ src/main/java/org/reservasApolaya/DawiCl3ApolayaRosalesGuillermoApplication.java
@@ -0,0 +1,13 @@
+package org.reservasApolaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3ApolayaRosalesGuillermoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3ApolayaRosalesGuillermoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/reservasApolaya/DawiCl3ApolayaRosalesGuillermoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reservasApolaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3ApolayaRosalesGuillermoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
